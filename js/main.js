9f23e8db8295fb6181d01fe8b2da29208e56b237
ChristopherMorriss <chrismorriss711@gmail.com>
2024-07-05 16:42:27 +0100

    Added JavaScript and hover effect to buttons

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ function validateForm(){
     let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
     //Above regex is used to try and validate all possible email addresses
     let email1 = $('#email').val();
-    let subject1 = $('#subject').val(); //Stores the subject value, but I do not use this info for anything yet
-    let textarea1 = $('#message-textarea').val(); //Stores the textarea value, but I do not use this info for anything yet
     let test1= regex.test(email1); //Checks if the email entered follows one of the regular expressions specified above
                                    //If it does, test1 will be assigned to true. If not, it will be assigned to false
     
@@ -21,4 +19,25 @@ function validateForm(){
       }
     }
   }
-  
+  let change=0;
+  var image_rand = document.querySelector("#random-img");
+  let image_btn = document.querySelector('#new-image');
+  image_btn.addEventListener('click',()=>{ //Only generates a single new image after pressing the button, need to fix this
+    change += 1;
+    alert('button pressed');
+    if (change >= 0){
+      //alert('yes')
+      image_rand.src="https://picsum.photos/500/500";
+    }
+  });
+
+function addImage(){
+  //image_rand.append('collection name')
+}
+
+function addEmail(){
+  select.innerHTML += `<option id ="${email1}" value="${email1}">${email1}</option>`;
+}
+// const SELECT = document.getElementById("select");
+
+
