4c7a25ad463fc479d31c66bb108d584fd0b8620c
ChristopherMorriss <chrismorriss711@gmail.com>
2024-07-08 15:41:31 +0100

    Improved 'New Collection' button functionality
    Also added 'Delete Collection button functionality'

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
+let email1 = $('#email').val();
 function validateForm(){
     let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
     //Above regex is used to try and validate all possible email addresses
+    //let email1 = $('#email').val();
     let email1 = $('#email').val();
+    alert(email1);
     let test1= regex.test(email1); //Checks if the email entered follows one of the regular expressions specified above
                                    //If it does, test1 will be assigned to true. If not, it will be assigned to false
     
@@ -18,29 +21,34 @@ function validateForm(){
         alert('Invalid email.');
       }
     }
+    const select = document.querySelector('#select');
+    function addEmail(){
+    select.innerHTML += `<option id="${email1}" value="${email1}">${email1}</option>`; 
+    //Directly adds the new HTML code to the first select tag (there is only one which is used for choosing the selection)
+    }
     addEmail();
   }
-  let change=0;
-  var image_rand = document.querySelector("#random-img");
-  let image_btn = document.querySelector('#new-image');
-  image_btn.addEventListener('click',()=>{ //Only generates a single new image after pressing the button, need to fix this
-    change += 1;
-    alert('button pressed');
-    if (change >= 0){
-      image_rand="https://picsum.photos/500/500";
-      $('#random-img').attr('src', image_rand + '?random=' + new Date().getTime()); 
-      //The above line is used because simply setting image_rand.src to the photo path only works once
-    }
+
+  //var image_rand = document.querySelector("#random-img");
+let image_btn = document.querySelector('#new-image');
+image_btn.addEventListener('click',()=>{ //Only generates a single new image after pressing the button, need to fix this
+  //alert('button pressed');
+  let image_rand="https://picsum.photos/500/500";
+  $('#random-img').attr('src', image_rand + '?random=' + new Date().getTime()); 
+  //The above line is used because simply setting image_rand.src to the photo path only works once
+    
   });
+// function addImage(){
+//     $('.img-collection').append(image_rand);
+//   } 
 
-function addImage(){
-  //image_rand.append('collection name')
-}
+let collection_deleter = document.querySelector('#delete');
+collection_deleter.addEventListener('click',()=>{
+  select.innerHTML -= `<option id="${email1}" value="${email1}">${email1}</option>`;
+});
 
-function addEmail(){
-  alert('function called')
-  let email2 = JSON.parse(localStorage.getItem("email1"));
-  select.innerHTML += `<option id ="${email2}" value="${email2}">${email2}</option>`;
-}
+
+
+
 
 
